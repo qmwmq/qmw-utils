6dff96b37032e3c80fbd77cd6397b3b0995de5f6
qmw <123349527@qq.com>
2025-05-09 22:09:30 +0800

    0

--- src/main/java/io/github/qmwmq/config/CustomP6SpyLogger.java
+++ src/main/java/io/github/qmwmq/config/CustomP6SpyLogger.java
@@ -5,17 +5,36 @@ import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * sql日志分析工具类
+ */
 public class CustomP6SpyLogger implements MessageFormattingStrategy {
 
+    /**
+     * 默认构造器
+     */
+    public CustomP6SpyLogger() {
+    }
+
+    /**
+     * @param connectionId 连接id
+     * @param currentTime  当前时间戳
+     * @param elapsed      sql执行时间
+     * @param category     类型
+     * @param preparedSql  带?的sql
+     * @param effectiveSql 带参数的完整sql
+     * @param url          数据库连接url
+     * @return 拼接的自定义sql
+     */
     @Override
     public String formatMessage(
             int connectionId,
-            String currentTime, // 时间戳
-            long elapsed, // 耗时
+            String currentTime,
+            long elapsed,
             String category,
-            String preparedSql, // 带?的sql
-            String effectiveSql, // 实际执行的sql
-            String url // 数据库连接url
+            String preparedSql,
+            String effectiveSql,
+            String url
     ) {
         if (effectiveSql.isEmpty())
             return "";
