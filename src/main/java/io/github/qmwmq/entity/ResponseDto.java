382749688e2e8ace8c931e2667c78f2f6f945e9a
qmw <123349527@qq.com>
2025-06-03 17:22:10 +0800

    0

--- src/main/java/io/github/qmwmq/entity/ResponseDto.java
+++ src/main/java/io/github/qmwmq/entity/ResponseDto.java
@@ -12,7 +12,7 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class ResponseDto<T> {
 
-    private Code code;
+    private int code;
     private String msg;
     private T data;
 
@@ -32,7 +32,7 @@ public class ResponseDto<T> {
      */
     public static <T> ResponseDto<T> ok(String msg, T data) {
         return new ResponseDto<T>()
-                .setCode(ConstantCode.OK)
+                .setCode(ConstantStatus.OK.code())
                 .setMsg(msg)
                 .setData(data);
     }
@@ -45,7 +45,7 @@ public class ResponseDto<T> {
      * @return ResponseDto
      */
     public static <T> ResponseDto<T> ok(T data) {
-        return ok(null, data);
+        return ok(ConstantStatus.OK.msg(), data);
     }
 
     /**
@@ -55,21 +55,21 @@ public class ResponseDto<T> {
      * @return ResponseDto
      */
     public static <T> ResponseDto<T> ok() {
-        return ok(null, null);
+        return ok(ConstantStatus.OK.msg(), null);
     }
 
     /**
      * 静态失败方法
      *
-     * @param code 失败码
-     * @param msg  信息
-     * @param data 有时失败也需要返回数据
-     * @param <T>  泛型
+     * @param status 失败状态
+     * @param msg    信息
+     * @param data   有时失败也需要返回数据
+     * @param <T>    泛型
      * @return ResponseDto
      */
-    public static <T> ResponseDto<T> error(Code code, String msg, T data) {
+    public static <T> ResponseDto<T> error(Status status, String msg, T data) {
         return new ResponseDto<T>()
-                .setCode(code)
+                .setCode(status.code())
                 .setMsg(msg)
                 .setData(data);
     }
@@ -77,13 +77,24 @@ public class ResponseDto<T> {
     /**
      * 静态失败方法
      *
-     * @param code 失败码
-     * @param msg  信息
-     * @param <T>  泛型
+     * @param status 失败状态
+     * @param msg    信息
+     * @param <T>    泛型
      * @return ResponseDto
      */
-    public static <T> ResponseDto<T> error(Code code, String msg) {
-        return error(code, msg, null);
+    public static <T> ResponseDto<T> error(Status status, String msg) {
+        return error(status, msg, null);
+    }
+
+    /**
+     * 静态失败方法
+     *
+     * @param status 失败状态
+     * @param <T>    泛型
+     * @return ResponseDto
+     */
+    public static <T> ResponseDto<T> error(Status status) {
+        return error(status, status.msg(), null);
     }
 
     /**
@@ -94,39 +105,59 @@ public class ResponseDto<T> {
      * @return ResponseDto
      */
     public static <T> ResponseDto<T> error(String msg) {
-        return error(null, msg, null);
+        return error(ConstantStatus.CUSTOM_ERROR, msg, null);
     }
 
     /**
      * 状态码接口，要扩展状态码需要enum该接口
      */
-    public interface Code {
+    public interface Status {
+        int code();
+        String msg();
     }
 
     /**
      * 常用状态码
      */
-    public enum ConstantCode implements Code {
+    public enum ConstantStatus implements Status {
         /**
          * 成功
          */
-        OK,
-        /**
-         * 自定义错误
-         */
-        CUSTOM_ERROR,
+        OK(1, "操作成功"),
         /**
          * 系统错误
          */
-        SYSTEM_ERROR,
+        SYSTEM_ERROR(1000, "系统错误"),
+        /**
+         * 自定义错误
+         */
+        CUSTOM_ERROR(1001, "操作失败"),
         /**
          * 需要登录
          */
-        LOGIN_REQUIRED,
+        LOGIN_REQUIRED(1002, "需要登录"),
         /**
          * 登录过期
          */
-        LOGIN_EXPIRED,
+        LOGIN_EXPIRED(1003, "登录过期");
+
+        private final int code;
+        private final String msg;
+
+        ConstantStatus(int code, String msg) {
+            this.code = code;
+            this.msg = msg;
+        }
+
+        @Override
+        public int code() {
+            return code;
+        }
+
+        @Override
+        public String msg() {
+            return msg;
+        }
     }
 
 }
