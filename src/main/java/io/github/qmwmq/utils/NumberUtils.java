c75a0fdaf9d0ad29909ab27dbe1c1be5c5aa8dcd
qmw <123349527@qq.com>
2025-11-28 10:49:11 +0800

    init

--- src/main/java/io/github/qmwmq/utils/NumberUtils.java
+++ src/main/java/io/github/qmwmq/utils/NumberUtils.java
@@ -5,6 +5,14 @@ import java.util.Set;
 
 public class NumberUtils {
 
+    public Long parseLong(String number) {
+        try {
+            return Long.parseLong(number);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
     // 两个非负整数映射到唯一非负整数，Cantor 配对函数
     public static long uniqueNumber(long a, long b) {
         if (a < 0 || b < 0)
