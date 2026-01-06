494b5a7697ad7e6fb5cf0056251c442881a0b7b2
qmwmq <123349527@qq.com>
2026-01-06 23:36:20 +0800

    0

---
+++ src/main/java/io/github/qmwmq/utils/JacksonUtils2.java
@@ -0,0 +1,169 @@
+//package io.github.qmwmq.utils;
+//
+//import lombok.Getter;
+//import lombok.SneakyThrows;
+//import tools.jackson.core.JsonGenerator;
+//import tools.jackson.core.JsonParser;
+//import tools.jackson.core.json.JsonReadFeature;
+//import tools.jackson.databind.*;
+//import tools.jackson.databind.json.JsonMapper;
+//import tools.jackson.databind.module.SimpleModule;
+//import tools.jackson.databind.type.CollectionType;
+//import tools.jackson.databind.type.MapType;
+//import tools.jackson.databind.type.TypeFactory;
+//
+//import java.io.IOException;
+//import java.math.BigDecimal;
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//import java.time.format.DateTimeFormatter;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+///**
+// * jackson相关工具类
+// */
+//public class JacksonUtils2 {
+//
+//    // https://blog.csdn.net/xueyijin/article/details/128892398
+//
+//    /**
+//     * 屏蔽默认构造器
+//     */
+//    private JacksonUtils2() {
+//    }
+//
+//    // 基础ObjectMapper
+//    @Getter
+//    private static final JsonMapper jsonMapper = JsonMapper.builder()
+//            .configure(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS, true) // 允许\r\n等
+//            .configure(JsonReadFeature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER, true) // 允许带反斜杠\
+//            .configure(JsonReadFeature.ALLOW_LEADING_ZEROS_FOR_NUMBERS, true) // 允许数字以0开头
+//            .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false) // 序列化无成员变量的对象会报错，这样配置直接序列化成{}
+//            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false) // 反序列化时忽略不存在的属性
+//            .changeDefaultPropertyInclusion() // 忽略null值的key
+//            .build()
+//            .registerModule(
+//                    new SimpleModule()
+//                            .addSerializer(BigDecimal.class, new JsonSerializer<>() {
+//                                @Override
+//                                public void serialize(BigDecimal value, JsonGenerator generator, SerializerProvider provider) throws IOException {
+//                                    generator.writeString(value.toPlainString());
+//                                }
+//                            })
+//                            .addSerializer(LocalDateTime.class, new JsonSerializer<>() {
+//                                @Override
+//                                public void serialize(LocalDateTime value, JsonGenerator generator, SerializerProvider provider) throws IOException {
+//                                    generator.writeString(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(value));
+//                                }
+//                            })
+//                            .addDeserializer(LocalDateTime.class, new JsonDeserializer<>() {
+//                                @Override
+//                                public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+//                                    String text = jsonParser.getText();
+//                                    return LocalDateTime.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+//                                }
+//                            })
+//                            .addSerializer(LocalDate.class, new JsonSerializer<>() {
+//                                @Override
+//                                public void serialize(LocalDate value, JsonGenerator generator, SerializerProvider provider) throws IOException {
+//                                    generator.writeString(value.toString());
+//                                }
+//                            })
+//                            .addDeserializer(LocalDate.class, new JsonDeserializer<>() {
+//                                @Override
+//                                public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+//                                    String text = jsonParser.getText();
+//                                    return LocalDate.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+//                                }
+//                            })
+//            );
+//
+//    /**
+//     * 解析为map
+//     *
+//     * @param o       入参
+//     * @param mapType map的类型
+//     * @param <K>     key
+//     * @param <V>     value
+//     * @return map
+//     */
+//    @SneakyThrows
+//    public static <K, V> Map<K, V> toMap(Object o, MapType mapType) {
+//        return objectMapper.readValue(o.toString(), mapType);
+//    }
+//
+//    /**
+//     * 解析为map
+//     *
+//     * @param o 入参
+//     * @return 出参
+//     */
+//    public static Map<String, Object> toMap(Object o) {
+//        return toMap(o, typeFactory.constructMapType(HashMap.class, String.class, Object.class));
+//    }
+//
+//    /**
+//     * 解析为对象
+//     *
+//     * @param o     入参
+//     * @param clazz 对象类
+//     * @param <T>   对象类泛型
+//     * @return 出参
+//     */
+//    @SneakyThrows
+//    public static <T> T toJavaObject(Object o, Class<T> clazz) {
+//        if (StringUtils.isBlank(o))
+//            o = "{}";
+//        return objectMapper.readValue(o.toString(), clazz);
+//    }
+//
+//    /**
+//     * 解析为list
+//     *
+//     * @param o       入参
+//     * @param mapType map类
+//     * @param <K>     map的key类型
+//     * @param <V>     map的value类型
+//     * @return 出参
+//     */
+//    @SneakyThrows
+//    public static <K, V> List<Map<K, V>> toList(Object o, MapType mapType) {
+//        if (StringUtils.isBlank(o))
+//            o = "[]";
+//        // 构建list的泛型
+//        CollectionType listType = typeFactory.constructCollectionType(List.class, mapType);
+//        return objectMapper.readValue(o.toString(), listType);
+//    }
+//
+//    /**
+//     * 解析为list
+//     *
+//     * @param o   入参
+//     * @param <K> Map的key类型
+//     * @param <V> Map的value类型
+//     * @return 出参
+//     */
+//    public static <K, V> List<Map<K, V>> toList(Object o) {
+//        // 构建map类型和key、value的泛型
+//        return toList(o, typeFactory.constructMapType(HashMap.class, String.class, Object.class));
+//    }
+//
+//    /**
+//     * 解析为list
+//     *
+//     * @param o     入参
+//     * @param clazz list元素类
+//     * @param <T>   list元素类泛型
+//     * @return 出参
+//     */
+//    @SneakyThrows
+//    public static <T> List<T> toList(Object o, Class<T> clazz) {
+//        if (StringUtils.isBlank(o))
+//            o = "[]";
+//        CollectionType listType = typeFactory.constructCollectionType(List.class, clazz);
+//        return objectMapper.readValue(o.toString(), listType);
+//    }
+//
+//}
--- src/main/java/io/github/qmwmq/utils/NumberUtils.java
+++ src/main/java/io/github/qmwmq/utils/NumberUtils.java
@@ -1,8 +1,5 @@
 package io.github.qmwmq.utils;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class NumberUtils {
 
     public Long parseLong(String number) {
@@ -18,22 +15,6 @@ public class NumberUtils {
         if (a < 0 || b < 0)
             throw new RuntimeException("both number must be non-negative");
         return (a + b) * (a + b + 1) / 2 + b;
-    }
-
-    public static void main(String[] args) {
-        Set<Long> ids = new HashSet<>();
-
-        long max = 0L;
-        for (int a = 0; a < 100000; a++) {
-            for (int b = 0; b < 100000; b++) {
-                max = Math.max(max, uniqueNumber(a, b));
-            }
-        }
-
-//        System.out.println(ids);
-        System.out.println(max);
-        System.out.println("end");
-
     }
 
 }
