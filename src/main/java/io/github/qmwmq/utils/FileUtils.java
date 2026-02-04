92f398a3ba6a9bb5068e49190d38d5fe2865fcf8
qmw <123349527@qq.com>
2026-02-04 10:38:23 +0800

    init

--- src/main/java/io/github/qmwmq/utils/FileUtils.java
+++ src/main/java/io/github/qmwmq/utils/FileUtils.java
@@ -1,5 +1,10 @@
 package io.github.qmwmq.utils;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.Objects;
+
 /**
  * 文件工具
  */
@@ -9,6 +14,18 @@ public class FileUtils {
      * 默认构造器
      */
     private FileUtils() {
+    }
+
+    public static String getFileType(String name) {
+        return name != null && name.contains(".") ? name.substring(name.lastIndexOf(".") + 1) : "";
+    }
+
+    public static String getFileType(File file) {
+        return getFileType(Objects.requireNonNull(file, "文件不能为空").getName());
+    }
+
+    public static String getFileType(MultipartFile file) {
+        return getFileType(Objects.requireNonNull(file, "文件不能为空").getOriginalFilename());
     }
 
 }
