a0a83a471a87e7aca327673dd5c76ce3c2dc77fa
qmw <123349527@qq.com>
2026-03-04 10:55:06 +0800

    init

--- src/main/java/io/github/qmwmq/utils/DateUtils.java
+++ src/main/java/io/github/qmwmq/utils/DateUtils.java
@@ -1,5 +1,9 @@
 package io.github.qmwmq.utils;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * 日期工具类
  */
@@ -29,5 +33,28 @@ public class DateUtils {
             "yyyy年M月d日",
             "yyyyMMdd",
     };
+
+    public static LocalDateTime parseDateTime(Object o) {
+        for (String pattern : patterns) {
+            try {
+                return LocalDateTime.parse(o.toString(), DateTimeFormatter.ofPattern(pattern));
+            } catch (Exception e) {
+                continue;
+            }
+        }
+        for (String pattern : patterns) {
+            try {
+                return LocalDate.parse(o.toString(), DateTimeFormatter.ofPattern(pattern)).atStartOfDay();
+            } catch (Exception e) {
+                continue;
+            }
+        }
+        return null;
+    }
+
+    public static LocalDate parseDate(Object o) {
+        LocalDateTime localDateTime = parseDateTime(o);
+        return localDateTime == null ? null : localDateTime.toLocalDate();
+    }
 
 }
