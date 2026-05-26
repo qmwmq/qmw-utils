217dec759f0c50d31abb4e533ffd4ca549f624cb
qmwmq <123349527@qq.com>
2026-05-27 00:04:23 +0800

    0

--- src/main/java/io/github/qmwmq/utils/CollectionUtils.java
+++ src/main/java/io/github/qmwmq/utils/CollectionUtils.java
@@ -1,4 +1,37 @@
 package io.github.qmwmq.utils;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+
 public class CollectionUtils {
+
+    /**
+     * 通用分批处理工具
+     *
+     * @param collection 要分批的集合
+     * @param batchSize  每批数量
+     * @param consumer   每批执行的逻辑
+     * @param <T>        任意类型
+     */
+    private <T> void batchProcess(Collection<T> collection, int batchSize, Consumer<Collection<T>> consumer) {
+        if (collection == null)
+            return;
+
+        List<T> list = collection.stream()
+                .filter(Objects::nonNull)
+                .toList();
+
+        if (list.isEmpty())
+            return;
+
+        int total = list.size();
+        for (int i = 0; i < total; i += batchSize) {
+            int end = Math.min(i + batchSize, total);
+            List<T> batch = list.subList(i, end);
+            consumer.accept(batch);
+        }
+    }
+
 }
